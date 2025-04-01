241c05ed8dd48687b0765e52ccf0cd1fb3fddced
tracyn09 <tracyn0099@gmail.com>
2025-04-01 08:58:27 -0400

    Task 2–Importing Greeting Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Greeting from './components/Greeting'
+import Greeting from './Greeting'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -30,7 +30,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
 
-      
+      <Greeting />
     </>
   )
 }
